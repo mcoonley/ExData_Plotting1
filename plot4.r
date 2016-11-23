99fe1bcb4b4e3ac5d4de4760eb81104be0137e7a
mcoonley <mlcoonley@gmail.com>
2016-11-23 12:52:58 -0500

    Add files via upload

---
+++ plot4.r
@@ -0,0 +1,17 @@
+housepwr<-read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
+                     na.strings = c("?","0"))
+png("plot4.png", height = 480, width = 480)
+date_sub<-subset(housepwr, Date %in% c("1/2/2007", "2/2/2007"))
+date_sub$DATE_TIME<-with(date_sub, paste0(Date, Time))
+re_date<-strptime(date_sub$DATE_TIME, format = "%d/%m/%Y%H:%M:%S")
+par(mfrow=c(2,2))
+plot(re_date, date_sub$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(re_date, date_sub$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+ plot(re_date, date_sub$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(re_date,date_sub$Sub_metering_2, type = "l", col = "red")
+lines(re_date,date_sub$Sub_metering_3, type = "l", col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+      col = c("black","red","blue"), lty = 1, cex = 0.75)
+plot(re_date, date_sub$Global_reactive_power, type = "l", xlab = "datetime", 
+       ylab = "Global_reactive_power")
+dev.off()
